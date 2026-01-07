ebf023687e492de7fa54b4d17e52ca71511d81df
Sacha-Martin <sacha.martin03@edu.univ-fcomte.fr>
2026-01-07 15:32:22 +0100

    avant publication

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// Menu
+
 const toggle = document.querySelector('.header__btn-menu');
 const closeBtn = document.querySelector('.header__close');
 const nav = document.querySelector('.menu');
@@ -14,3 +16,34 @@ closeBtn.addEventListener('click', () => {
     nav.classList.toggle('open');
     page.classList.toggle('no-scroll');
 });
+
+
+// Carousel
+
+const carousel = document.querySelector("div.carousel__container");
+const prevButton = document.querySelector("button.carousel__button--prev");
+const nextButton = document.querySelector("button.carousel__button--next");
+
+if (carousel) {
+    const premierItem = document.querySelector(".carousel__item");
+
+    function getScrollAmount() {
+        return premierItem.clientWidth;
+    }
+
+    prevButton.addEventListener("click", () => {
+        carousel.scrollBy({
+            left: -getScrollAmount(),
+            behavior: "smooth"
+        });
+    });
+
+    nextButton.addEventListener("click", () => {
+        carousel.scrollBy({
+            left: getScrollAmount(),
+            behavior: "smooth"
+        });
+    });
+    window.addEventListener("resize", () => {
+    });
+}
